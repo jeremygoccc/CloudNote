73e6330bb0f5faed2d3cd9b094c539ad0e6e387c
fxbabys <zhuguogen@qq.com>
2018-02-06 21:23:15 +0800

    Initial commit

---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+//index.js
+//获取应用实例
+
+Page({
+  data: {
+    mylists: [
+        {
+            id: 1,
+            title: "学习",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nulla ut, dicta reiciendis adipisci officia nemo. Nostrum eos, molestias modi vero, voluptates nulla eligendi saepe tenetur, nesciunt, debitis sit fuga.",
+            time: "18:32"
+        },
+        {
+            id: 2,
+            title: "学习",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, ullam suscipit! Cumque debitis recusandae iste in laudantium culpa aliquid ea inventore commodi, laboriosam natus esse impedit? Iusto voluptatem magni, quisquam.",
+            time: "15:12"
+        },
+        {
+            id: 3,
+            title: "学习",
+            content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, ullam suscipit! Cumque debitis recusandae iste in laudantium culpa aliquid ea inventore commodi, laboriosam natus esse impedit? Iusto voluptatem magni, quisquam.",
+            time: "15:12"
+        },
+    ]
+  },
+})
